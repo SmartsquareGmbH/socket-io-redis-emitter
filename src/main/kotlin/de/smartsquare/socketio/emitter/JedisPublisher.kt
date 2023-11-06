0ccb0635620acf5aa5a9a39dfa4a01bb951e8086
Daniel Rosowski <rosowski@smartsquare.de>
2023-11-06 16:02:54 +0100

    Use JedisPool instead of single connection

--- src/main/kotlin/de/smartsquare/socketio/emitter/JedisPublisher.kt
+++ src/main/kotlin/de/smartsquare/socketio/emitter/JedisPublisher.kt
@@ -1,9 +1,9 @@
 package de.smartsquare.socketio.emitter
 
-import redis.clients.jedis.Jedis
+import redis.clients.jedis.JedisPool
 
-class JedisPublisher(private val jedis: Jedis) : RedisPublisher {
+class JedisPublisher(private val jedisPool: JedisPool) : RedisPublisher {
     override fun publish(channel: String, message: ByteArray) {
-        jedis.use { it.publish(channel.toByteArray(), message) }
+        jedisPool.resource.use { it.publish(channel.toByteArray(), message) }
     }
 }
